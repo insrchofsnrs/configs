29bbee21f8f7f0b2c423482d78eae3db1535feb4
smile <e.vavilkin@gmail.com>
2022-10-26 14:34:08 +0400

    pi script added

---
+++ vps/raspberry-pi/start.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+echo "let's setup this damn server"
+sudo apt update
+sudo apt -yq --allow upgrade
+sudo apt -yq --allow autoclean
+sudo apt -yq --allow autoremove
+
+sudo apt install -yq --allow mc screenfetch net-tools ncdu
+
+sudo snap install docker
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+sudo sh <(wget -qO - https://downloads.nordcdn.com/apps/linux/install.sh)
+nordvpn login --legacy
+nordvpn whitelist add subnet 192.168.0.0/16
+nordvpn whitelist add port 22
+nordvpn set technology NordLynx
+nordvpn set autoconnect on spain
+nordvpn c
+
+mkdir bit-torrent && cd bit-torrent
+mkdir config torrents downloads
+sudo chown 520 config torrents downloads
+cat > docker-compose.yaml <<EOF
+version: "3.9"
+
+services:
+  torrent:
+    container_name: 'bit-torrent'
+    image: andreipoe/qbittorrent-aarch64
+    volumes:
+       - $PWD/config:/config
+       - $PWD/torrents:/torrents
+       - $PWD/downloads:/downloads
+       - /etc/timezone:/etc/timezone:ro
+       - /etc/localtime:/etc/localtime:ro
+    ports:
+      - "80:8080"
+      - "6881:6881/tcp"
+      - "6881:6881/udp"
+    restart: always
+EOF
+docker compose up -d
