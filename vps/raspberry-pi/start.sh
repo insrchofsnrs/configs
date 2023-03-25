2f72e3026087d92907db92b586ff4c53feca2587
smile <e.vavilkin@gmail.com>
2023-03-25 20:19:13 +0400

    some changes

--- vps/raspberry-pi/start.sh
+++ vps/raspberry-pi/start.sh
@@ -6,59 +6,68 @@ sudo apt update
 sudo apt -yq upgrade
 sudo apt -yq autoclean
 sudo apt -yq autoremove
-sudo apt install -yq mc screenfetch net-tools ncdu samba docker.io docker-compose
+sudo apt install -yq mc screenfetch net-tools ncdu
+
+echo "Install docker"
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -yq
+
+
 echo "chto to ne to"
 # sudo groupadd docker
 sudo usermod -aG docker $USER
 echo 'posle sudo usermod -aG docker $USER'
 # sudo newgrp docker
-echo "do nordvpn"
+
+
+echo "Install nordvpn"
 wget -qO - https://downloads.nordcdn.com/apps/linux/install.sh | sudo bash
-echo "posle nordvpn"
 sudo nordvpn login --legacy
 sudo nordvpn whitelist add subnet 192.168.0.0/16
 sudo nordvpn whitelist add port 22
 sudo nordvpn set technology NordLynx
 sudo nordvpn set autoconnect on spain
+sudo nordvpn set meshnet on
 sudo nordvpn c spain
+
+echo "Creating folders for qbittorrent"
 mkdir bit-torrent
 mkdir share
 sudo chown 520 share
 cd bit-torrent
 mkdir config torrents
 sudo chown 520 config torrents
-cat > docker-compose.yaml <<EOF
-version: "3.3"
 
-services:
-  torrent:
-    container_name: 'bit-torrent'
-    image: andreipoe/qbittorrent-aarch64
-    volumes:
-       - $PWD/config:/config
-       - $PWD/torrents:/torrents
-       - /home/pi/share:/downloads
-       - /etc/timezone:/etc/timezone:ro
-       - /etc/localtime:/etc/localtime:ro
-    ports:
-      - "80:8080"
-      - "6881:6881/tcp"
-      - "6881:6881/udp"
-    restart: always
+echo "starting cloning telegram bot for bittorrent"
+git clone https://github.com/insrchofsnrs/qbittorrent-telegram-bot
+cd qbittorrent-telegram-bot
+docker buildx build --platform linux/arm64/v8 -t torrent-bot:latest .
+cd ..
 
-EOF
-sudo docker-compose up -d
-cd /home/pi
-sudo systemctl start smbd
-sudo systemctl enable smbd
-sudo tee -a /etc/samba/smb.conf <<EOF
-[share]
-    comment = Samba on Ubuntu
-    path = /home/pi/share
-    read only = no
-    browsable = yes
-EOF
-sudo service smbd restart
-sudo smbpasswd -a pi
+
+echo "Starting samba docker"
+
+echo "Starting install samba share in docker"
+sudo docker run -d \
+    -p 139:139 \
+    -p 445:445 \
+    --restart always \
+    --name samba\
+    -v /home/pi/share:/share \
+    elswork/samba \
+    -u "$(id -u):$(id -g):$(id -un):$(id -gn):123" \
+    -s "share:/share:rw:$(id -un)"
 
 sudo reboot
